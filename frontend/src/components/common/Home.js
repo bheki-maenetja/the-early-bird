65d544533869634f6c25f42c4717176bb780cce7
bheki-maenetja <bhekimaenetja@gmail.com>
2020-04-06 11:30:04 +0200

    layouting for top news page

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -2,36 +2,12 @@ import React from 'react'
 
 class Home extends React.Component {
 
-  state = {
-    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum libero venenatis pellentesque varius. Aliquam erat volutpat. Donec commodo eu felis sed convallis. Suspendisse posuere justo ac nisl pretium condimentum. Nunc eleifend velit quis erat pharetra, non pretium tellus pulvinar. Morbi sit amet porttitor sem, a ornare risus. Sed blandit pellentesque pharetra. Sed at enim rhoncus, cursus sem et, consectetur sapien. Morbi sit amet enim neque. Quisque pharetra dolor non neque pretium, a aliquet diam dapibus. Maecenas metus enim, sollicitudin in ipsum pretium, sagittis rutrum turpis. Pellentesque non massa in nulla accumsan accumsan. Integer gravida massa et ullamcorper tempus. Suspendisse sollicitudin ligula metus, et bibendum urna hendrerit in. Etiam sodales leo lobortis dignissim dignissim. Aliquam at quam sit amet lacus sagittis scelerisque ornare vel ex.'
-  }
+  state = {}
 
   render() {
-    const { text } = this.state
     return (
       <>   
-      <section className="section" style={{ flexGrow: '1', overflowY: 'scroll' }}>
-        <div className="container">
-          <div className="tile is-ancestor">
-            <div className="tile is-parent is-7">
-              <div className="tile is-child box">
-                <h1 className="title is-1">Main Story</h1>
-                <p>{text}</p>
-              </div>
-            </div>
-            <div className="tile is-vertical is-parent">
-              <div className="tile is-child box">
-                <h2 className="title is-4">Secondary Story</h2>
-                <p>{text}</p>
-              </div>
-              <div className="tile is-child box">
-                <h2 className="title is-4">Secondary Story</h2>
-                <p>{text}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <h1>Home Page</h1>
       </>
     )
   }
--- frontend/src/components/news/TopNews.js
+++ frontend/src/components/news/TopNews.js
@@ -1,13 +1,58 @@
 import React from 'react'
+import axios from 'axios'
 
 class TopNews extends React.Component {
 
-  state = {}
+  state = {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum libero venenatis pellentesque varius. Aliquam erat volutpat. Donec commodo eu felis sed convallis. Suspendisse posuere justo ac nisl pretium condimentum. Nunc eleifend velit quis erat pharetra, non pretium tellus pulvinar. Morbi sit amet porttitor sem, a ornare risus. Sed blandit pellentesque pharetra. Sed at enim rhoncus, cursus sem et, consectetur sapien. Morbi sit amet enim neque. Quisque pharetra dolor non neque pretium, a aliquet diam dapibus. Maecenas metus enim, sollicitudin in ipsum pretium, sagittis rutrum turpis. Pellentesque non massa in nulla accumsan accumsan. Integer gravida massa et ullamcorper tempus. Suspendisse sollicitudin ligula metus, et bibendum urna hendrerit in. Etiam sodales leo lobortis dignissim dignissim. Aliquam at quam sit amet lacus sagittis scelerisque ornare vel ex.'
+  }
 
   render() {
+    const { text } = this.state
     return (
       <>
-      <h1>TopNews</h1>
+      <section className="section" style={{ flexGrow: '1', overflowY: 'scroll' }}>
+        <div className="container">
+          <div className="tile is-ancestor">
+            <div className="tile is-vertical is-parent is-7">
+              <div className="tile is-child box" style={{ height: 'fit-content' }}>
+                <h1 className="title is-1">Main Story</h1>
+                <p>{text}</p>
+              </div>
+              <div className="tile">
+                <div className="tile is-parent">
+                  <div className="tile is-child box">
+                    <h3 className="title is-5">Tertiary Story</h3>
+                    <p>{text}</p>
+                  </div>
+                </div>
+                <div className="tile is-parent">
+                  <div className="tile is-child box">
+                    <h3 className="title is-5">Tertiary Story</h3>
+                    <p>{text}</p>
+                  </div>
+                </div>
+                <div className="tile is-parent">
+                  <div className="tile is-child box">
+                    <h3 className="title is-5">Tertiary Story</h3>
+                    <p>{text}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="tile is-vertical is-parent" style={{ height: 'fit-content' }}>
+              <div className="tile is-child box" >
+                <h2 className="title is-4">Secondary Story</h2>
+                <p>{text}</p>
+              </div>
+              <div className="tile is-child box">
+                <h2 className="title is-4">Secondary Story</h2>
+                <p>{text}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       </>
     )
   }
