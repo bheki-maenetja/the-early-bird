385be8feb28dfa2bea244d2406797e5b9316c579
bheki-maenetja <bhekimaenetja@gmail.com>
2020-04-09 09:44:42 +0200

    added functionality to follow and unfollow publishers on publishers page

--- frontend/src/components/publishers/Publishers.js
+++ frontend/src/components/publishers/Publishers.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import axios from 'axios'
 
+import Authorize from '../../lib/authorize'
+
 const newsApiKey = process.env.REACT_APP_NEWS_API_KEY
 
 class Publishers extends React.Component {
   state = {
+    userData: null,
     sources: null,
   }
 
   async componentDidMount() {
     try {
-      const res = await axios.get(`https://newsapi.org/v2/sources?&apiKey=${newsApiKey}`)
-      console.log(res.data)
-      this.setState({ sources: res.data.sources })
+      const res = await Promise.all([
+        axios.get('/api/users/my-profile/', {
+          headers: {
+            Authorization: `Bearer ${Authorize.getToken()}`
+          }
+        }),
+        axios.get(`https://newsapi.org/v2/sources?&apiKey=${newsApiKey}`)
+      ])
+      this.setState({ sources: res[1].data.sources, userData: res[0].data })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  refreshPage = async () => {
+    try {
+      const res = await axios.get('/api/users/my-profile/', {
+        headers: {
+          Authorization: `Bearer ${Authorize.getToken()}`
+        }
+      })
+      this.setState({ userData: res.data })
     } catch (err) {
       console.log(err)
     }
@@ -23,10 +45,55 @@ class Publishers extends React.Component {
     this.getSources(e.target.value)
   }
 
+  followPublisher = async (source) => {
+    const pubData = {
+      name: source.name,
+      slug: source.id,
+      description: source.description,
+      source_url: source.url
+    }
+
+    try {
+      const res = await axios.post('/api/users/publishers/', pubData, {
+        headers: {
+          Authorization: `Bearer ${Authorize.getToken()}`
+        }
+      })
+      console.log(res)
+      this.refreshPage()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  unfollowPublisher = async (source) => {
+    const { userData } = this.state
+    const chosenSource = userData.favourite_publishers.find(pub => pub.slug === source.id)
+    
+    try {
+      const res = await axios.delete('/api/users/publishers/', {
+        headers: {
+          Authorization: `Bearer ${Authorize.getToken()}`
+        },
+        data: {
+          publisherId: chosenSource.id
+        }
+      })
+      console.log(res)
+      this.refreshPage()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   render() {
-    const { sources } = this.state
-    if (!sources) return false
-    console.log(this.state)
+    const { sources, userData } = this.state
+    console.log(sources)
+    let userPubs
+    if (sources && userData) {
+      userPubs = userData.favourite_publishers.map(pub => pub.slug)
+    }
+    console.log(userData)
     return (
       <>
       <section className="section" style={{ flexGrow: '1', overflowY: 'scroll' }}>
@@ -34,27 +101,44 @@ class Publishers extends React.Component {
           <h1 className="title is-2 has-text-centered">Explore the World's Most Trusted News Sources</h1>
           <hr />
           <div className="columns is-mobile is-multiline">
-            {sources.map(source => (
-              <>
-              <div className="column is-one-third-desktop is-half-tablet is-full-mobile">
-                <div className="box" style={{ height: '100%' }}>
-                  <h2 className="title is-3">{source.name}</h2>
-                  <hr />
-                  <p className="subtitle is-5">{source.description}</p>
-                  <div className="level is-mobile">
-                    <div className="level-left">
-                      <a href={source.url} target="_blank">
-                        <i className="fas fa-external-link-alt fa-2x has-text-dark"></i>
-                      </a>
-                    </div>
-                    <div className="level-right">
-                      <button className="button is-info">Follow</button>
+            {sources && userData ? 
+            <>
+              {sources.map(source => (
+                <>
+                <div className="column is-one-third-desktop is-half-tablet is-full-mobile">
+                  <div className="box" style={{ height: '100%' }}>
+                    <h2 className="title is-3">{source.name}</h2>
+                    <hr />
+                    <p className="subtitle is-5">{source.description}</p>
+                    <div className="level is-mobile">
+                      <div className="level-left">
+                        <a href={source.url} target="_blank">
+                          <i className="fas fa-external-link-alt fa-2x has-text-dark"></i>
+                        </a>
+                      </div>
+                      <div className="level-right">
+                        {userPubs.includes(source.id) ?
+                        <>
+                          <button className="button is-danger" onClick={() => this.unfollowPublisher(source)}>Unfollow</button>
+                        </>
+                        :
+                        <>
+                          <button className="button is-info" onClick={() => this.followPublisher(source)}>Follow</button>
+                        </> 
+                        }
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-              </>
-            ))}
+                </>
+              ))}
+            </>
+            :
+            <>
+            <h1 className="title is-4">Loading...</h1>
+            <progress class="progress is-success" max="100">30%</progress>
+            </> 
+            }
           </div>
         </div>
       </section>
